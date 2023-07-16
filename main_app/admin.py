9b356a9e2f7e292f0d13b88eebd2d54c19103b3e
rlegler2012 <rlegler2012@hotmail.com>
2023-07-16 10:02:16 -0700

    location model added connected to professional model so you can pick locations for certain professionals

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Professional 
+from .models import Professional, Location
 
 admin.site.register(Professional)
+admin.site.register(Location)
--- main_app/models.py
+++ main_app/models.py
@@ -12,3 +12,15 @@ class Professional(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+class Location(models.Model):
+
+    venue = models.CharField(max_length=300)
+    bio = models.TextField(max_length=800)
+    professional = models.ForeignKey(Professional, on_delete=models.CASCADE, related_name="locations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.venue
+
