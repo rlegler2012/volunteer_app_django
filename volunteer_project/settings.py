70794b4241e4a5d72a0f271ed7f0395333f4e257
rlegler2012 <rlegler2012@hotmail.com>
2023-07-19 12:02:18 -0700

    updated settings.py

--- volunteer_project/settings.py
+++ volunteer_project/settings.py
@@ -99,7 +99,6 @@ WSGI_APPLICATION = 'volunteer_project.wsgi.application'
 
 DATABASES = {
       'default': dj_database_url.config(
-        default=f"postgres://{os.environ['DB_USER']}:{os.environ['DB_PW']}@{os.environ['DB_HOST']}/dentalvolunteer",
         conn_max_age=600,
         conn_health_checks=True,
     )
